6b43522760147d0bfc948f965bdf5eff2fcb27aa
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-16 17:24:40 -0400

    [Raghav]added variables in mobile class

--- app/src/main/java/com/king/c0780996_w2020_mad3125_fp/ModelClasses/Mobile.java
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_fp/ModelClasses/Mobile.java
@@ -1,5 +1,24 @@
 package com.king.c0780996_w2020_mad3125_fp.ModelClasses;
 
-public class Mobile
-{
+import org.joda.time.LocalDate;
+
+public class Mobile extends Bill {
+
+    String manufacturerName;
+    String planName;
+    String mobileNumber;
+    Integer mobGbUsed;
+    Integer minutesUsed;
+
+    public Mobile(String billId, LocalDate billDate, BillType billType, String manufacturerName, String planName, String mobileNumber, Integer mobGbUsed, Integer minutesUsed)
+    {
+        super(billId, billDate, billType);
+        this.manufacturerName = manufacturerName;
+        this.planName = planName;
+        this.mobileNumber = mobileNumber;
+        this.mobGbUsed = mobGbUsed;
+        this.minutesUsed = minutesUsed;
+        this.billTotal = billCalculate();
+    }
+
 }
