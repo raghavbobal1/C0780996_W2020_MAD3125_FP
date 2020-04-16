2dd35cfc44ea51ba28f0a7f267f0bf43a54ded02
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-16 17:16:04 -0400

    [Raghav]completed internet class

--- app/src/main/java/com/king/c0780996_w2020_mad3125_fp/ModelClasses/Internet.java
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_fp/ModelClasses/Internet.java
@@ -2,15 +2,56 @@ package com.king.c0780996_w2020_mad3125_fp.ModelClasses;
 
 import org.joda.time.LocalDate;
 
-public class Internet extends Bill {
+public class Internet extends Bill
+{
     private String providerName;
     private Double gbUsed;
 
-    public Internet(String billId, LocalDate billDate, BillType billType, String providerName, Double gbUsed) {
+    public Internet(String billId, LocalDate billDate, BillType billType, String providerName, Double gbUsed)
+    {
         super(billId, billDate, billType);
         this.providerName = providerName;
         this.gbUsed = gbUsed;
         this.billTotal = billCalculate();
     }
 
+    @Override
+    public Double billCalculate(){
+        double price = 0.0;
+        if( gbUsed < 3)
+        {
+            price = 12.15 * gbUsed;
+        }
+        else if (gbUsed > 3 && gbUsed < 10)
+        {
+            price = 14.0 * gbUsed;
+        }
+        else
+            {
+            price = 15.15 * gbUsed;
+            }
+        return price;
+    }
+
+    public String getProviderName()
+    {
+        return providerName;
+    }
+
+    public void setProviderName(String providerName)
+    {
+        this.providerName = providerName;
+    }
+
+    public Double getGbUsed()
+    {
+        return gbUsed;
+    }
+
+    public void setGbUsed(Double gbUsed)
+    {
+        this.gbUsed = gbUsed;
+    }
 }
+
+
