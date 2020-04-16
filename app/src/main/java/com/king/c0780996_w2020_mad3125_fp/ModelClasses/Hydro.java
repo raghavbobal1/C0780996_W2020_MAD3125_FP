222d9de29bbd9ddbda13ea500ee5fdaabec73e9d
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-16 16:56:43 -0400

    [Raghav]added variables in hydro class

--- app/src/main/java/com/king/c0780996_w2020_mad3125_fp/ModelClasses/Hydro.java
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_fp/ModelClasses/Hydro.java
@@ -1,6 +1,18 @@
 package com.king.c0780996_w2020_mad3125_fp.ModelClasses;
 
-public class Hydro
+import org.joda.time.LocalDate;
+
+public class Hydro extends Bill
 {
-    
+    private String agencyName;
+    private Integer unitsConsumed;
+
+    public Hydro(String billId, LocalDate billDate, BillType billType, String agencyName, Integer unitsUsed){
+        super(billId, billDate, billType);
+        this.agencyName = agencyName;
+        this.unitsConsumed = unitsUsed;
+        this.billTotal = billCalculate();
+    }
+
+   
 }
