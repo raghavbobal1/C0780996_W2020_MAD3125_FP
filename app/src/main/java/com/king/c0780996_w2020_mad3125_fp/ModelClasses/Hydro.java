de64c3c293e2994b7acc00618673193ce680885f
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-16 17:00:05 -0400

    [Raghav]completed hydro class

--- app/src/main/java/com/king/c0780996_w2020_mad3125_fp/ModelClasses/Hydro.java
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_fp/ModelClasses/Hydro.java
@@ -7,12 +7,58 @@ public class Hydro extends Bill
     private String agencyName;
     private Integer unitsConsumed;
 
-    public Hydro(String billId, LocalDate billDate, BillType billType, String agencyName, Integer unitsUsed){
+    public Hydro(String billId, LocalDate billDate, BillType billType, String agencyName, Integer unitsConsumed)
+    {
         super(billId, billDate, billType);
         this.agencyName = agencyName;
-        this.unitsConsumed = unitsUsed;
+        this.unitsConsumed = unitsConsumed;
         this.billTotal = billCalculate();
     }
 
-   
+    @Override
+    public Double billCalculate()
+    {
+        double price = 0.0;
+        double hydroAmt = 0.0;
+        if (unitsConsumed < 60)
+        {
+            price += 150;
+        }
+        else if(unitsConsumed > 600 && unitsConsumed <= 800)
+        {
+            price += 215;
+        }
+        else if (unitsConsumed > 800 && unitsConsumed < 1000)
+        {
+            price += 300;
+        }
+        else
+        {
+            price = 0.75 * unitsConsumed;
+        }
+        hydroAmt = price ;
+        return hydroAmt;
+    }
+
+
+    public String getAgencyName()
+    {
+        return agencyName;
+    }
+
+    public void setAgencyName(String agencyName)
+    {
+        this.agencyName = agencyName;
+    }
+
+    public Integer getUnitsUsed()
+    {
+        return unitsConsumed;
+    }
+
+    public void setUnitsUsed(Integer unitsConsumed)
+    {
+        this.unitsConsumed = unitsConsumed;
+    }
+
 }
