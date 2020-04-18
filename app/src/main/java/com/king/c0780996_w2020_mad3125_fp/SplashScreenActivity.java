f35020931fd96f269ad41aab6e4e6ee86bfef0c7
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-18 05:42:56 -0400

    [Raghav]added login activity

--- app/src/main/java/com/king/c0780996_w2020_mad3125_fp/SplashScreenActivity.java
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_fp/SplashScreenActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class splash_screen_activity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen_activity);
+        setContentView(R.layout.activity_splash_screen);
     }
 }
