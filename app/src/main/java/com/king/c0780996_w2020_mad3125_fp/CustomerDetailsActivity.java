d63acab36e5c9f8b020e7a28ddb0da70e5861bb9
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-18 23:48:35 -0400

    [Raghav]created a new activity to show the list of customers

---
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_fp/CustomerDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.king.c0780996_w2020_mad3125_fp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_details);
+    }
+}
