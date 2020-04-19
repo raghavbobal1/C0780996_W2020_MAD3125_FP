6449f0e85fbfbfe4478c3b364a6c65edcf6e3e81
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-19 06:12:16 -0400

    [Raghav]added a new bill total activity

---
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_fp/BillTotalActivity.java
@@ -0,0 +1,14 @@
+package com.king.c0780996_w2020_mad3125_fp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BillTotalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill_total);
+    }
+}
--- app/src/main/java/com/king/c0780996_w2020_mad3125_fp/CustomerListActivity.java
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_fp/CustomerListActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CustomerDetailsActivity extends AppCompatActivity {
+public class CustomerListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_details);
+        setContentView(R.layout.activity_customer_list);
     }
 }
