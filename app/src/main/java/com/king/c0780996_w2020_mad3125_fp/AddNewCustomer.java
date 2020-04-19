efb44d1e4d5a7a4d146bf0feb3a1ac462e275103
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-19 01:21:33 -0400

    [Raghav]added some textviews

---
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_fp/AddNewCustomer.java
@@ -0,0 +1,14 @@
+package com.king.c0780996_w2020_mad3125_fp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_customer);
+    }
+}
