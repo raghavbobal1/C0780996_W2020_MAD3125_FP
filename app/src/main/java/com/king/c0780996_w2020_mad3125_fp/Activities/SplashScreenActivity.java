e1c027631c812c0822f2be1f8ec02a051e91df64
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-20 02:31:19 -0400

    [Raghav]completed splash screen activity class

--- app/src/main/java/com/king/c0780996_w2020_mad3125_fp/Activities/SplashScreenActivity.java
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_fp/Activities/SplashScreenActivity.java
@@ -1,16 +1,34 @@
 package com.king.c0780996_w2020_mad3125_fp.Activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.king.c0780996_w2020_mad3125_fp.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_splash_screen);
+
+            ActionBar mActionBar = getSupportActionBar();
+            mActionBar.hide();
+
+            handler=new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent=new Intent(SplashScreenActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            },5000);
+        }
     }
-}
