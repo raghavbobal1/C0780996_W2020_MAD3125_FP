f97254e0f17ae66212cc41f5f6476a6544e158e6
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-20 03:45:32 -0400

    [Raghav]added method for date picker

--- app/src/main/java/com/king/c0780996_w2020_mad3125_fp/Activities/AddNewCustomerActivity.java
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_fp/Activities/AddNewCustomerActivity.java
@@ -56,4 +56,69 @@ public class AddNewCustomerActivity extends AppCompatActivity {
     private String gender;
 
 
-  
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_customer);
+        ButterKnife.bind(this);
+
+        ActionBar mActionBar = getSupportActionBar();
+        mActionBar.hide();
+
+        addingDatePicker();
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fieldChecker();
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearfields();
+            }
+        });
+    }
+
+    private void addingDatePicker()
+    {
+        editDOBtext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        AddNewCustomerActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                String date ="";
+                month = month + 1;
+                String monthName = getMonthName(month);
+                if(day<10) {
+                    date = "0"+day + "-" + monthName + "-" + year;
+                }
+                else
+                {
+                    date = day + "-" + monthName + "-" + year;
+                }
+                editDOBtext.setText(date);
+            }
+        };
+    }
+
+    
