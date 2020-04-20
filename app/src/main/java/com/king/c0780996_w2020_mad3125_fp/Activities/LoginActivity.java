051885a36e093e08552ef43da64d4ee06c8a7543
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-20 02:25:39 -0400

    [Raghav]completed login activity class

--- app/src/main/java/com/king/c0780996_w2020_mad3125_fp/Activities/LoginActivity.java
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_fp/Activities/LoginActivity.java
@@ -1,23 +1,19 @@
 package com.king.c0780996_w2020_mad3125_fp.Activities;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Paint;
-import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputEditText;
@@ -33,7 +29,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.concurrent.Executor;
 
 public class LoginActivity extends AppCompatActivity {
         private TextInputLayout editEmail;
@@ -61,6 +56,7 @@ public class LoginActivity extends AppCompatActivity {
 
         Integer firstflag = 1;
 
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
         @Override
         protected void onCreate(Bundle savedInstanceState)
         {
