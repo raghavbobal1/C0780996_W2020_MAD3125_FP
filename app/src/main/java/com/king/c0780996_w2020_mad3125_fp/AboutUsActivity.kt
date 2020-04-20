74521ebc2781c833d5eccfbc93af63200c5744f2
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-20 01:49:08 -0400

    [Raghav]added about us class in KOTLIN

---
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_fp/AboutUsActivity.kt
@@ -0,0 +1,12 @@
+package com.king.c0780996_w2020_mad3125_fp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutUsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_us)
+    }
+}
