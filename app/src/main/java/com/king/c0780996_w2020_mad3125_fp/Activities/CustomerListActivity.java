fbfa9a17531453b662c604e190a17d00ab2d11d4
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-20 04:18:35 -0400

    [Raghav]declared variables in cust list act class

--- app/src/main/java/com/king/c0780996_w2020_mad3125_fp/Activities/CustomerListActivity.java
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_fp/Activities/CustomerListActivity.java
@@ -1,16 +1,39 @@
 package com.king.c0780996_w2020_mad3125_fp.Activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import com.king.c0780996_w2020_mad3125_fp.Adapters.CustomerAdapter;
+import com.king.c0780996_w2020_mad3125_fp.ModelClasses.Customer;
 import com.king.c0780996_w2020_mad3125_fp.R;
+import com.king.c0780996_w2020_mad3125_fp.Utility.DataStorage;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class CustomerListActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_list);
-    }
+
+    private RecyclerView customerRV;
+    private ArrayList customerArrayList;
+    private ArrayList tempCustomerArrayList;
+    private CustomerAdapter customerAdapter;
+    private ImageView barAddCustomer;
+    private ImageView barLogout;
+
+    @BindView(R.id.btnAddCustomer) Button btnAddCustomer;
+
 }
