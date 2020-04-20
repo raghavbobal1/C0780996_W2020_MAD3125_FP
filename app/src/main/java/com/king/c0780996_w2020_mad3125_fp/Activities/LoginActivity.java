3f817a7d0694b40fa8f03d1ef61ae1f01376d95e
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-20 09:11:58 -0400

    [Raghav] added style for rem me switch

--- app/src/main/java/com/king/c0780996_w2020_mad3125_fp/Activities/LoginActivity.java
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_fp/Activities/LoginActivity.java
@@ -1,19 +1,24 @@
 package com.king.c0780996_w2020_mad3125_fp.Activities;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Paint;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputEditText;
@@ -29,118 +34,161 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.concurrent.Executor;
 
-public class LoginActivity extends AppCompatActivity {
-        private TextInputLayout editEmail;
-        private TextInputEditText editEmailText;
+public class LoginActivity extends AppCompatActivity
+{
+    private TextInputEditText edtEmailIdText;
+    private TextInputEditText edtPasswordText;
+    private TextInputLayout edtEmailId;
+    private TextInputLayout edtPassword;
+    private Button btnlogin;
+    private Switch swchRememberMe;
+    private TextView txtAboutUs;
+    public static ArrayList<String> emailList = new ArrayList<>();
+    public static ArrayList<String> passwordList = new ArrayList<>();
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
-        private TextInputEditText editPasswordText;
-        private TextInputLayout editPassword;
+    public static final String PREFS_NAME = "MyPrefsFile";
+    private static final String PREF_USERNAME = "username";
+    private static final String PREF_PASSWORD = "password";
 
-        private Button btnSignIn;
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
+    MediaPlayer mp;
+    Integer someflag = 1;
 
-        private Switch switchRembMe;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AndroidThreeTen.init(this);
 
-        private TextView aboutUsText;
-
-        public static ArrayList<String> emailList = new ArrayList<>();
-
-        public static ArrayList<String> passwordList = new ArrayList<>();
-
-        SharedPreferences sharedPreferences;
-        SharedPreferences.Editor editor;
-
-        public static final String PREFS_NAME = "MyPrefsFile";
-        private static final String PREF_USERNAME = "username";
-        private static final String PREF_PASSWORD = "password";
-
-        Integer firstflag = 1;
-
-        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-        @Override
-        protected void onCreate(Bundle savedInstanceState)
-        {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            AndroidThreeTen.init(this);
-            ActionBar mActionBar = getSupportActionBar();
-            mActionBar.hide();
-
-            editEmailText = findViewById(R.id.editEmailText);
-            editPasswordText = findViewById(R.id.editPasswordText);
-            btnSignIn = findViewById(R.id.btnSignIn);
-            switchRembMe = findViewById(R.id.switchRembMe);
-            editEmail = findViewById(R.id.editEmail);
-            editPassword = findViewById(R.id.editPassword);
-            aboutUsText = findViewById(R.id.aboutUsText);
-
-            aboutUsText.setPaintFlags(aboutUsText.getPaintFlags() |   Paint.UNDERLINE_TEXT_FLAG);
-            aboutUsText.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent mIntent = new Intent(LoginActivity.this, AboutUsActivity.class);
-                    startActivity(mIntent);
-                }
-            });
-            sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-            editor = sharedPreferences.edit();
-
-            String email = sharedPreferences.getString(PREF_USERNAME, null);
-            String password = sharedPreferences.getString(PREF_PASSWORD, null);
-            editEmailText.setText(email);
-            editPasswordText.setText(password);
-
-            try {
-                JSONObject obj = new JSONObject(loadJSONFromAsset("LoginInformation.json"));
-                JSONArray userArray = obj.getJSONArray("users");
-                for (int i = 0; i < userArray.length(); i++)
-                {
-                    JSONObject userInfo = userArray.getJSONObject(i);
-                    emailList.add(userInfo.getString("email"));
-                    passwordList.add(userInfo.getString("password"));
-                }
+        // BIOMETRIC CODE
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(LoginActivity.this,
+                executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode,
+                                              @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication error: " + errString, Toast.LENGTH_SHORT)
+                        .show();
             }
-            catch (JSONException e)
+
+            @Override
+            public void onAuthenticationSucceeded(
+                    @NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication succeeded!", Toast.LENGTH_SHORT).show();
+                successfulLogin();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                invalidFinger();
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric login for my app")
+                .setSubtitle("Log in using your biometric credential")
+                .setNegativeButtonText("Use password")
+                .build();
+
+        // BIOMETRIC CODE END
+
+        ActionBar mActionBar = getSupportActionBar();
+        mActionBar.hide();
+
+        edtEmailIdText = findViewById(R.id.editEmailText);
+        edtPasswordText = findViewById(R.id.editPasswordText);
+        btnlogin = findViewById(R.id.btnSignIn);
+        swchRememberMe = findViewById(R.id.switchRembMe);
+        edtEmailId = findViewById(R.id.editEmail);
+        edtPassword = findViewById(R.id.editPassword);
+        txtAboutUs = findViewById(R.id.aboutUsText);
+
+        txtAboutUs.setPaintFlags(txtAboutUs.getPaintFlags() |   Paint.UNDERLINE_TEXT_FLAG);
+        txtAboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(LoginActivity.this, AboutUsActivity.class);
+                startActivity(mIntent);
+            }
+        });
+        sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        String email = sharedPreferences.getString(PREF_USERNAME, null);
+        String password = sharedPreferences.getString(PREF_PASSWORD, null);
+        edtEmailIdText.setText(email);
+        edtPasswordText.setText(password);
+
+        try {
+            JSONObject obj = new JSONObject(loadJSONFromAsset("LoginInformation.json"));
+            JSONArray userArray = obj.getJSONArray("users");
+            for (int i = 0; i < userArray.length(); i++)
             {
-                e.printStackTrace();
+                JSONObject userInfo = userArray.getJSONObject(i);
+                emailList.add(userInfo.getString("email"));
+                passwordList.add(userInfo.getString("password"));
             }
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
 
-            btnSignIn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-                    if(editEmailText.getText().toString().isEmpty())
+                if(edtEmailIdText.getText().toString().isEmpty())
+                {
+                    edtEmailIdText.setError("Enter your email address");
+                }
+
+                if(edtPasswordText.getText().toString().isEmpty())
+                {
+                    edtPasswordText.setError("Enter your password");
+                    return;
+                }
+
+                for(int i=0, j =0; i<emailList.size(); i++,j++)
+                {
+                    if(emailList.get(i).equals(edtEmailIdText.getText().toString()) && passwordList.get(i).equals(edtPasswordText.getText().toString()))
                     {
-                        editEmailText.setError("Email id left blank");
-                    }
-
-                    if(editPasswordText.getText().toString().isEmpty())
-                    {
-                        editPasswordText.setError("Password left blank");
+                        if(swchRememberMe.isChecked()){
+                            editor.putString(PREF_USERNAME,edtEmailIdText.getText().toString());
+                            editor.putString(PREF_PASSWORD,edtPasswordText.getText().toString());
+                            editor.apply();
+                        }
+                        else
+                        {
+                            editor.putString(PREF_USERNAME,"");
+                            editor.putString(PREF_PASSWORD,"");
+                            editor.apply();
+                        }
+                        if(someflag == 1) {
+                            biometricPrompt.authenticate(promptInfo);
+                            someflag = 0;
+                            return;
+                        }
+                        successfulLogin();
+                        someflag = 1;
                         return;
                     }
-
-                    for(int i=0, j =0; i<emailList.size(); i++,j++)
-                    {
-                        if(emailList.get(i).equals(editEmailText.getText().toString()) && passwordList.get(i).equals(editPasswordText.getText().toString()))
-                        {
-                            if(switchRembMe.isChecked()){
-                                editor.putString(PREF_USERNAME,editEmailText.getText().toString());
-                                editor.putString(PREF_PASSWORD,editPasswordText.getText().toString());
-                                editor.apply();
-                            }
-                            else
-                            {
-                                editor.putString(PREF_USERNAME,"");
-                                editor.putString(PREF_PASSWORD,"");
-                                editor.apply();
-                            }
-                        }
-                    }
-                    loginFail();
                 }
-            });
-        }
+                invalidLogin();
+            }
+        });
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public String loadJSONFromAsset(String filename) {
@@ -159,18 +207,33 @@ public class LoginActivity extends AppCompatActivity {
         return json;
     }
 
-    public void loginPass()
+    public void successfulLogin()
     {
+        mp.start();
         Intent mIntent = new Intent(LoginActivity.this, CustomerListActivity.class);
         startActivity(mIntent);
         return;
     }
 
-    public void loginFail()
+    public void invalidLogin()
     {
         new MaterialAlertDialogBuilder(LoginActivity.this)
                 .setTitle("Invalid username or password")
-                .setMessage("Incorrect information entered.")
+                .setMessage("Please check the information you entered")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+        return;
+    }
+    public void invalidFinger()
+    {
+        new MaterialAlertDialogBuilder(LoginActivity.this)
+                .setTitle("Invalid fingerprint")
+                .setMessage("Please try again")
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -181,4 +244,3 @@ public class LoginActivity extends AppCompatActivity {
         return;
     }
 }
-
